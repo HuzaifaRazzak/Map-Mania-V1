caf52234f18ddab9cdc0b4a6798b3df9d8cdace5
HuzaifaRazzak <huzaifaarazzak@lewisu.edu>
2023-10-26 14:48:00 -0500

    Requirement 3 Update

--- static/map-mania-v1.js
+++ static/map-mania-v1.js
@@ -5,10 +5,12 @@ function initMap() {
         center: { lat: 41.878, lng: 10 }, zoom: 3
     });
 
-    var marker = new google.maps.Marker({ position: { lat: 45.3306, lng: -91.4918 }, map: gMap });
+    //var marker = new google.maps.Marker({ position: { lat: 45.3306, lng: -91.4918 }, map: gMap });
 
     var marker2 = new google.maps.Marker({ position: { lat: 36.3932, lng: 25.4615 }, map: gMap });
     marker2.setIcon('https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png');
+
+    var marker3 = new google.maps.Marker({ position: { lat: 12.432, lng: 43.234 }, map: gMap });
 
     var infoWindow = new google.maps.InfoWindow({ content: 'Santorini, Greece' });
     marker2.addListener('click', function () {
@@ -20,12 +22,13 @@ function initMap() {
     });
 
     function updateGame() {
-        console.log('function updateGame() google-maps-step-03!');
+        console.log('function updateGame()');
         var zoomLevel = gMap.getZoom();
         var inBounds = false;
-        var boundsChanged = gMap.getBounds();
+        var boundsChanged = true;
 
-        if (gMap.getBounds().contains({ lat: 45.3306, lng: -91.4918 })) {
+        //if (gMap.getBounds().contains({ lat: 45.3306, lng: -91.4918 })) {
+        if (gMap.getBounds().contains({ lat: 12.432, lng: 43.234 })){
             inBounds = true;
         }
 
